8ac67eaca83b9e09ffcb1acafe18cd2170627a5c
zhaoshaoting <item233@163.com>
2020-08-06 10:53:45 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
+<!--App.vue是根组件，表示其他的组件被这个组件包含-->
+<!-- .vue的文件，结构上类似html，一个.vue文件就是一个vue组件-->
 <template>
+<!--  这里的id="app" 只和下面的样式对应，与index.html文件中的id="app"不同 -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/lunch.jpg">
+    <!--src/components/ChooseLunch.vue 组件-->
+    <HelloWorld msg="Choose Lunch"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/ChooseLunch.vue'
 
+// export default 是ES6语法。意思是将这个组件整体导出，之后就可以使用import导入组件，{} 中的内容是这个组件相关属性
 export default {
   name: 'App',
   components: {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 'vue' 是jar包
+// './App.vue' 是我们自己编写的组件
+// 该文件是App.vue和index.html的纽带
+// Vue.config.productionTip = false 作用：阻止vue在启动时产生成产提示
 Vue.config.productionTip = false
 
+// 创建一个Vue对象（实例）
+// .$mount('#app')挂载到页面上DOM元素
+// h => h(App), 表示该对象包含的 Vue组件
 new Vue({
   render: h => h(App),
 }).$mount('#app')
